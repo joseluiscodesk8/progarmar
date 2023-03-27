d370efb036efcba522d1a5fd6bef72ede4d06c90
jose luis gomez angarita <74441811+joseluiscodesk8@users.noreply.github.com>
2023-03-26 21:14:27 -0500

    lostcanvas2

--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,27 @@ let options;
 let attack;
 let attackEnemy = [];
 let knightPlayer;
+
+let buttons = [];
+
 // CHARTERS
 
-const charter = document.getElementById("charter");
+const charter = document.getElementById("charter_player");
+const charter_enemy = document.getElementById("chater_enemy");
 const charterPlayer = document.getElementById("charter-player");
 const charterEnemy = document.getElementById("charter-enemy");
+
+// BUTTONS OF ATTACK
+let button_one = document.getElementById("button_one");
+let button_two = document.getElementById("button_two");
+let button_three = document.getElementById("button_three");
+let button_four = document.getElementById("button_four");
+console.log(button_one);
+
+// CHOOSE
+
+let choose_charter = document.getElementById("button-charter");
+choose_charter.addEventListener("click", selectionKnights);
 
 //  LIFES
 
@@ -28,12 +44,11 @@ function playerKnights() {
     options = `
   <section class="soilder">
             <img src=${knights.img}  alt=${knights.name}/>
-            <label for="shion">${knights.name}</label>
+            <label for="">${knights.name}</label>
             <input type="radio" name="charter" id=${knights.name} />
         </section>
   `;
     charter.innerHTML += options;
-    console.log(charter);
   });
 }
 
@@ -42,11 +57,21 @@ function playerKnights() {
 playerKnights();
 
 function enemyCharter() {
-  let aleatorio = selection(0, knights.length - 1);
-
-  charterEnemy.innerHTML = knights[aleatorio].name;
-
-  console.log(charterEnemy);
+  // let aleatorio = selection(0, knights.length - 1);
+  // charterEnemy.innerHTML = knights[aleatorio].name;
+  // let img = document.createElement("img");
+  // img.style.width = "100px";
+  // img.src = "../../public/aries.jpg";
+  // charter_enemy.appendChild(img);
+  // let img2 = document.createElement("img");
+  // img2.style.width = "100px";
+  // if (aleatorio == 0) {
+  //   img.src = "../../public/aries.jpg";
+  //   charter_enemy.appendChild(img);
+  // } else if (aleatorio == 1) {
+  //   img2.src = "../../public/tauro.jpg";
+  //   charter_enemy.appendChild(img2);
+  // }
 }
 
 function selectionKnights() {
@@ -56,17 +81,19 @@ function selectionKnights() {
   let hideAttack = document.getElementById("attack");
   hideAttack.style.display = "flex";
 
-  if (shion.checked) {
-    charterPlayer.innerHTML = aries.id;
-    knightPlayer = aries.id;
-  } else if (hasgrad.checked) {
-    charterPlayer.innerHTML = tauro.id;
-    knightPlayer = tauro.id;
-  } else {
-    alert("elije attack");
-  }
+  // if (shion.checked == shion) {
+  //   charterPlayer.innerHTML = aries.id;
+  //   knightPlayer = aries.id;
+  //   console.log(charterPlayer);
+  // }
+  // } else if (hasgrad.checked) {
+  //   charterPlayer.innerHTML = tauro.id;
+  //   knightPlayer = tauro.id;
+  // } else {
+  //   alert("elije attack");
+  // }
 
-  enemyCharter();
+  // enemyCharter();
 }
 
 // selectionKnights();
